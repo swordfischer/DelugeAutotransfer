6ae340a5d62a125070067097cf61a6f3fbcb1f7d
Mickey William Fischer <mwf@swordfischer.com>
2013-01-29 19:27:54 +0100

    Added FTP ability

--- DelugeAutotransfer.pl
+++ DelugeAutotransfer.pl
@@ -25,34 +25,37 @@ open (STDERR, ">>", $logfile);
 open (LOG, ">>",  $logfile) or die "Couldn't open $logfile: $!\n";
 
 print LOG "$date - [COMPLETED] $torrent_path/$torrent_name\n"; 
-## Here we go!
 for my $users (sort keys %{$connections}) {
 	my $userblock 	= $connections->{$users};
 	my $login	= $userblock->{'login'};
+	my $password	= $userblock->{'password'};
 	my $destination = $userblock->{'destination'};
 	my $host	= $userblock->{'host'};
+	my $port	= $userblock->{'port'};
 	my $transfer	= $userblock->{'transfer'};
 	my $email	= $userblock->{'email'};
+	my $transmethod = $userblock->{'method'};
 	
-	## Iterate over all the RegExes
 	for (sort @{$transfer}) {
 		for my $match_name (sort keys %{$_}) {
 			my $snatch  	=	$_->{$match_name}->{match};
 			my $location	=	$_->{$match_name}->{location};
-			## Clean up the regex key, and the username, convert _ to spaces.
 			$match_name 	=~ 	tr/_/ /;
 			$users		=~	tr/_/ /;
 			if ( $torrent_name =~ m/$snatch/i ) {
-				## Setup the mail subject and content
 				my $subject 	=	"Torrent Transferred ($match_name)";
 				my $content	=	"Hello $users.\n$torrent_name has been successfully transferred to you.";
-				## If there is an alternate download location, change the destination to match this.
 				if ($location) { $destination = $destination."$location/"; }
-				## Start RSYNC'ing
-				&rsync($torrent_path, $torrent_name, $login, $host, $destination)
-					or die "$date - [FAILED] - $torrent_name to $users\n";
-				print LOG "$date - [TRANSFERRED] $torrent_name to $users\n";
-				## Might as well send an email if the user has one.
+
+				if ($transmethod =~ m/ftp/) {
+				&ftp($torrent_path, $torrent_name, $login, $password, $host, $destination)
+					or die "$date - [FAILED] ($transmethod) - $torrent_name to $users\n";
+				}
+				elsif ($transmethod =~ m/rsync/) {
+				&rsync($torrent_path, $torrent_name, $login, $host, $port, $destination)
+					or die "$date - [FAILED] ($transmethod) - $torrent_name to $users\n";
+				}
+				print LOG "$date - [TRANSFERRED] ($transmethod) - $torrent_name to $users\n";
 				&send_mail($email, $subject, $content ) if ($email);
 				
 			} 
--- functions.pl
+++ functions.pl
@@ -4,6 +4,7 @@ use strict;
 use utf8;
 use Net::SMTP::SSL;
 use File::Rsync;
+use Net::FTP;
 
 sub send_mail {
 my $to          =       $_[0];
@@ -33,16 +34,36 @@ if (not $smtp = Net::SMTP::SSL->new('smtp.gmail.com',
 1;
 }
 sub rsync {
-my $rsync       =       File::Rsync->new( {'recursive' => '1' } );
 my $spath	=	$_[0];
 my $sname 	=	$_[1];
 my $duser 	=	$_[2];
 my $dhost	=	$_[3];
-my $ddest	=	$_[4];
+my $dport	=	$_[4];
+my $ddest	=	$_[5];
+my $rsync       =       File::Rsync->new( {'recursive' => '1' } );
 $rsync->exec( {
        		src     =>      "$spath/$sname",                                                                        
         	dest    =>      "$duser\@$dhost:$ddest",                                                                         
+		port	=>	"$dport",
 	} ) or die return 0;
+1;
+}
+sub ftp {
+my $spath       =       $_[0];
+my $sname       =       $_[1];                                                                                                         
+my $duser       =       $_[2];
+my $dpass	=	$_[3];                                                                                                      
+my $dhost       =       $_[4];                                                                                                         
+my $ddest       =       $_[5];                                                                                                         
+my $ftp		=	Net::FTP->new("$dhost", Debug => 0)
+			or die "Cannot connect to $dhost: $@";
+$ftp		->	login("$duser", "$dpass")
+			or die "Cannot login ", $ftp->message;
+$ftp		->	cwd("$ddest")
+			or die "Cannot change working directory ", $ftp->message;
+$ftp		->	put("$spath/$sname")
+			or die "Cannot put ", $ftp->message;
+$ftp		->	quit;
 1;
 }
 1;   
